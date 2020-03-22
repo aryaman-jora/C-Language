95ed113163edf391c97fbb0c3517ef6f78c674d6
aryaman-jora <62513654+aryaman-jora@users.noreply.github.com>
2020-03-22 22:31:38 +0530

    Add files via upload

---
+++ armstrong.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int digit(int n)
+{ int i;
+	for(i=0;n!=0;i++)
+	{
+		
+	}
+}
+
+
+
+main()
+{
+	int n,temp;
+	printf("enter number");
+	scanf("%d",&n);
+	temp=n;
+	digit(n);
+}
